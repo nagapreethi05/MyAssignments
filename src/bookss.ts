b8c07bf6ff97bd1096b8803e23991090523485dd
nagapreethi05 <nagapreethi05@gmail.com>
2021-04-12 18:42:53 +0530

    multiPage

---
+++ src/bookss.ts
@@ -0,0 +1,16 @@
+class Bookss1 {
+    id: number;
+    title: string;
+    author: string;
+    price: number;
+    rating: number;  
+
+    constructor(id:number,title:string,author:string,price:number,rating:number) {
+    this.id = id;
+    this.title=title;
+    this.author=author;
+    this.price= price;
+    this.rating= rating;
+    
+    }
+}
