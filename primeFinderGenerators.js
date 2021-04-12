4beb5d752d0b8c66199e28e198ab238051daca42
nagapreethi05 <51773616+nagapreethi05@users.noreply.github.com>
2021-04-12 19:17:47 +0530

    GeneratorPrimeFinder

---
+++ primeFinderGenerators.js
@@ -0,0 +1,26 @@
+function* genPrime(n) {
+
+    if ( n % 1 || n < 2)
+        return " Number not valid : " + n;
+    
+        for (var i = 2; i < n; i++) {
+            if (isPrime(i)) {
+                yield i;
+            }
+        }
+    }
+    
+    
+    function isPrime(num) {
+        for (var i = 2; i < num; i++) {
+            if (num % i === 0) {
+                return false;
+            }
+        }
+        return true;
+    }
+    const generatorObject = genPrime(10);
+    console.log(generatorObject.next().value);
+    console.log(generatorObject.next().value);
+    console.log(generatorObject.next().value);
+    console.log(generatorObject.next().value);
