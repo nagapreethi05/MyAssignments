c81ea76e2aa30ef4cd3e18dee35f2cb661179d96
nagapreethi05 <51773616+nagapreethi05@users.noreply.github.com>
2021-04-13 21:36:14 +0530

    MultiPageAll

---
+++ MultiPageBook/public/addbooks.js
@@ -0,0 +1,8 @@
+"use strict";
+// function addbook(){
+//     let newid=document.getElementById("newid").value;
+//     let newtitle=document.getElementById("newtitle").value;
+//     let newauthor=document.getElementById("newauthor").value;
+//     let newprice=document.getElementById("newprice").value;
+//     let newrating=document.getElementById("newrating").value;
+// }
---
+++ MultiPageBook/public/bookss.js
@@ -0,0 +1,10 @@
+"use strict";
+class Bookss1 {
+    constructor(id, title, author, price, rating) {
+        this.id = id;
+        this.title = title;
+        this.author = author;
+        this.price = price;
+        this.rating = rating;
+    }
+}
