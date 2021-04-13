6ea87fd1171560f523c1e7e88cf985ac523129a8
nagapreethi05 <51773616+nagapreethi05@users.noreply.github.com>
2021-04-13 21:48:53 +0530

    Delete public directory

--- public/addbooks.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-// function addbook(){
-//     let newid=document.getElementById("newid").value;
-//     let newtitle=document.getElementById("newtitle").value;
-//     let newauthor=document.getElementById("newauthor").value;
-//     let newprice=document.getElementById("newprice").value;
-//     let newrating=document.getElementById("newrating").value;
-// }
